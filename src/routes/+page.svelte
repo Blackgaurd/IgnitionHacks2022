4a9c8de15bf61bee97fd15f69aa8915bed4d10dd
bryan <bryan.deng002@gmail.com>
2022-08-27 13:41:27 -0400

    yes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+  import Calculator from './Calculator.svelte';
+
+  import {Theme} from 'carbon-components-svelte';
+  import 'carbon-components-svelte/css/all.css';
+</script>
+
+<div class="absolute">
+  <Theme
+    render="toggle"
+    toggle={{
+      themes: ['g10', 'g90'],
+      labelA: '☀️',
+      labelB: '🌙'
+    }}
+    persist
+    persistKey="__carbon-theme"
+  />
+</div>
+
+<Calculator />
+
+<!--todo: add a bounce or some type of animation to the arrow-->
+<div
+  class="absolute left-1/2 bottom-3 z-20 w-3 h-3 border-b-2 border-r-2 border-solid rotate-45 border-white"
+/>
+
+<!-- content -->
+<h1>What are sleep cycles?</h1>
---
+++ src/routes/Calculator.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import {
+    Dropdown,
+    TimePicker,
+    TimePickerSelect,
+    SelectItem,
+    Button
+  } from 'carbon-components-svelte';
+  import 'carbon-components-svelte/css/all.css';
+</script>
+
+<div class="flex h-screen">
+  <div class="m-auto">
+    <!--come up with a better name-->
+    <h1 class="my-5">Sleep Calculator</h1>
+
+    <div class="flex flex-row space-x-4">
+      <div>
+        <Dropdown
+          selectedId="sleep"
+          items={[
+            {id: 'sleep', text: 'I want to sleep at...'},
+            {id: 'wake', text: 'I want to wake up at...'}
+          ]}
+        />
+      </div>
+
+      <div>
+        <TimePicker
+          hideLabel
+          placeholder="hh:mm"
+          invalidText="A valid value is required"
+        >
+          <TimePickerSelect value="pm">
+            <SelectItem value="am" text="AM" />
+            <SelectItem value="pm" text="PM" />
+          </TimePickerSelect>
+        </TimePicker>
+      </div>
+    </div>
+
+    <div class="my-3">
+      <Button kind="secondary">Calculate</Button>
+    </div>
+  </div>
+</div>
